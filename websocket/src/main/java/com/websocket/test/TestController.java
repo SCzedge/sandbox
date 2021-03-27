af07ab1debe346497fd1544aff153b8f747003cd
SCzedge <sczedge@gmail.com>
2021-03-28 02:34:31 +0900

    working on websocket

--- websocket/src/main/java/com/websocket/test/TestController.java
+++ websocket/src/main/java/com/websocket/test/TestController.java
@@ -2,10 +2,11 @@ package com.websocket.test;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
+	
 	@MessageMapping("/hello")
 	@SendTo("/topic/greetings")
 	public TestVo test() {
