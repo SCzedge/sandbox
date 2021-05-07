deb4a27745a828c9b9f3127c9db1de9d67594f01
sczedge <sczedge@gmail.com>
2021-05-07 14:27:34 +0900

    done kafka

---
+++ email/src/main/java/com/email/SendEmail.java
@@ -0,0 +1,40 @@
+package com.email;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+
+import javax.mail.internet.MimeMessage;
+
+public class SendEmail {
+    /*
+    private final JavaMailSenderImpl mailSender;
+    @Autowired
+    public SendEmail(JavaMailSenderImpl mailSender) {
+        this.mailSender = mailSender;
+    }
+*/
+    @Autowired
+    private JavaMailSenderImpl mailSender;
+
+
+    public void sending() {
+        final MimeMessagePreparator preparator = new MimeMessagePreparator() {
+            @Override
+            public void prepare(MimeMessage mimeMessage) throws Exception {
+                MimeMessageHelper helper;
+                helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+                helper.setFrom("homePage <factoryTest@gmail.com>");
+                helper.setTo("yjcho@youhost.co.kr");
+                helper.setSubject("EmailTest");
+
+                String body="TEST_BODY";
+                helper.setText(body,true);
+
+            }
+        };
+        mailSender.send(preparator);
+    }
+
+}
