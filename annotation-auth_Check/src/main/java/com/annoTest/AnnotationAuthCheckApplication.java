a0de4365a7465c2611f991028234d6467dd5f883
sczedge <sczedge@gmail.com>
2021-07-12 14:43:28 +0900

    initial annotation_auth check project

---
+++ annotation-auth_Check/src/main/java/com/annoTest/AnnotationAuthCheckApplication.java
@@ -0,0 +1,13 @@
+package com.annoTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationAuthCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnotationAuthCheckApplication.class, args);
+	}
+
+}
---
+++ annotation-auth_Check/src/test/java/com/annoTest/AnnotationAuthCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annoTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnotationAuthCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
