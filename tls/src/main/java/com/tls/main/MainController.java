da9db4ed550e063220d6d161da8af8742cc6772b
sczedge <sczedge@gmail.com>
2021-07-30 11:20:42 +0900

    add https

--- tls/src/main/java/com/tls/main/MainController.java
+++ tls/src/main/java/com/tls/main/MainController.java
@@ -1,4 +1,17 @@
 package com.tls.main;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
 public class MainController {
+
+    @GetMapping("/")
+    public String main() {
+        String uuid = UUID.randomUUID().toString();
+        System.out.println(uuid);
+        return uuid;
+    }
 }
