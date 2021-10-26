ecc9c68a81986098ed43c1b53480fa46afa64d60
sczedge <sczedge@gmail.com>
2021-10-26 17:53:15 +0900

    add interface KStreamProcessor

---
+++ springStream/src/main/java/com/springstream/basic/KStreamProcessor.java
@@ -0,0 +1,14 @@
+package com.springstream.basic;
+
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
+
+public interface KStreamProcessor {
+
+        @Input("input")
+        KStream<?, ?> input();
+
+        @Output("output")
+        KStream<?, ?> output();
+}
--- springStream/src/main/java/com/springstream/basic/basicListener.java
+++ springStream/src/main/java/com/springstream/basic/basicListener.java
@@ -7,6 +7,10 @@ import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.SendTo;
 
 import java.util.Arrays;
@@ -14,18 +18,10 @@ import java.util.Arrays;
 @Slf4j
 @EnableBinding(KStreamProcessor.class)
 public class basicListener {
+
+
     @StreamListener("input")
     @SendTo("output")
-    public KStream<?, WordCount> process(KStream<?, String> input) {
-        return input
-                .flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
-                .groupBy((key, value) -> value)
-                .windowedBy(TimeWindows.of(5000))
-                .count(Materialized.as("WordCounts-multi"))
-                .toStream()
-                .map((key, value) -> new KeyValue<>(null, new WordCount(key.key(), value, new Date(key.window().start()), new Date(key.window().end()))));
-    }
-
     public KStream<?, ?> process(KStream<?, String> input) {
         input.peek((K,V)-> log.info("key: {}, value: {}",K,V));
 
@@ -34,10 +30,8 @@ public class basicListener {
                 .groupBy((key, value) -> value)
                 .windowedBy(TimeWindows.of(5000))
                 .count(Materialized.as("WordCounts-multi"))
-                .toStream()
-                .map((key, value) -> new KeyValue<>(null, new WordCount(key.key(), value, new Date(key.window().start()), new Date(key.window().end()))));
+                .toStream();
     }
-
 
 
 }
